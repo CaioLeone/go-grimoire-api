1b29cdab282483f80ce1d71bed78d074db5d8ad1
Caio <caio_leone@hotmail.com>
2026-04-01 10:08:57 -0300

    feat: init handlers

--- api/handlers.go
+++ api/handlers.go
@@ -1,1 +1,27 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type PostBody struct {
+	URL string `json:url`
+}
+
+type Response struct {
+	Error string `json:"error,omitempty"`
+	Data  any    `json:"data,omitempty"`
+}
+
+func newHandler(db map[string]string) http.Handler {
+	r := chi.NewMux()
+	
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	return r
+}
--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+
+}
+
+func run() error {
+	db := make(map[string]string)
+	handler := NewHandler(db)
+
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
