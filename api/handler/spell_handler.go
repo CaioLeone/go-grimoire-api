d8c52b0a4ab0b1497bb776fc841339ed13c83fe6
Caio <caio_leone@hotmail.com>
2026-04-06 11:10:16 -0300

    feat: spell handlers func

--- api/handler/spell_handler.go
+++ api/handler/spell_handler.go
@@ -6,19 +6,11 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/google/uuid"
 
 	domApi "github.com/caioleone/go-grimoire-api/api/domain"
 	repoApi "github.com/caioleone/go-grimoire-api/api/repository"
 )
-
-type PostBodySpell struct {
-	URL string `json:url`
-}
-
-type ResponseSpell struct {
-	Error string `json:"error,omitempty"`
-	Data  any    `json:"data,omitempty"`
-}
 
 func NewHandlerSpell(repoSpell *repoApi.SpellRepository) http.Handler {
 	r := chi.NewMux()
@@ -50,23 +42,89 @@ func handleCreateSpell(repo *repoApi.SpellRepository) http.HandlerFunc {
 		var spell domApi.SpellModel
 
 		if err := json.NewDecoder(r.Body).Decode(&spell); err != nil {
+			SendJson(w, Response{Error: "Invalid Body"}, http.StatusBadRequest)
 			return
 		}
+
+		if len(spell.Name) < 2 || len(spell.Description) < 2 || len(spell.Element) < 2 || spell.ManaCost <= 0 {
+			SendJson(w, Response{Error: "Invalid Fields"}, http.StatusBadRequest)
+			return
+		}
+
+		created := repo.Insert(spell)
+		SendJson(w, Response{Data: created}, http.StatusCreated)
 	}
 }
 
 func handleGetSpell(repo *repoApi.SpellRepository) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		spell := repo.FindAll()
+		SendJson(w, Response{Data: spell}, http.StatusOK)
+	}
 }
 
 func handleGetByIdSpell(repo *repoApi.SpellRepository) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			SendJson(w, Response{Error: "Invalid Id"}, http.StatusBadRequest)
+			return
+		}
+
+		spell, ok := repo.FindById(id)
+		if !ok {
+			SendJson(w, Response{Error: "Spell Not Found"}, http.StatusBadRequest)
+		}
+
+		SendJson(w, Response{Data: spell}, http.StatusOK)
+	}
 }
 
 func handleUpdateSpell(repo *repoApi.SpellRepository) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			SendJson(w, Response{Error: "Invalid Id"}, http.StatusBadRequest)
+			return
+		}
+
+		var updated domApi.SpellModel
+		if err := json.NewDecoder(r.Body).Decode(&updated); err != nil {
+			SendJson(w, Response{Error: "Invalid Body"}, http.StatusBadRequest)
+			return
+		}
+
+		result, ok := repo.Update(id, updated)
+		if !ok {
+			SendJson(w, Response{Error: "Spell Not Found"}, http.StatusNotFound)
+			return
+		}
+
+		SendJson(w, Response{Data: result}, http.StatusOK)
+	}
 }
 
 func handleDeleteSpell(repo *repoApi.SpellRepository) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			SendJson(w, Response{Error: "Invalid Id"}, http.StatusBadRequest)
+			return
+		}
+
+		deleted, ok := repo.Delete(id)
+		if !ok {
+			SendJson(w, Response{Error: "User Not Found"}, http.StatusNotFound)
+			return
+		}
+
+		SendJson(w, Response{Data: deleted}, http.StatusOK)
+	}
 }
