4e8c6f6707d50161c5bef5683d85019b9803dee8
Caio <caio_leone@hotmail.com>
2026-04-09 09:34:32 -0300

    fix: Spell and Creature Usecases

--- api/usecase/creatureUsecase.go
+++ api/usecase/creatureUsecase.go
@@ -1,13 +1,18 @@
 package api
 
 import (
+	domApi "github.com/caioleone/go-grimoire-api/api/domain"
 	repoApi "github.com/caioleone/go-grimoire-api/api/repository"
 )
 
-type CreatureUsecase struct{
-	repo repoApi.MemoryCreatureRepository
+type CreatureUsecase struct {
+	repo repoApi.CreatureRepository
 }
 
-func NewCreatureUseCase(repo repoApi.MemoryCreatureRepository) *CreatureUsecase{
+func NewCreatureUseCase(repo repoApi.CreatureRepository) *CreatureUsecase {
 	return &CreatureUsecase{repo: repo}
+}
+
+func (u *CreatureUsecase) CreateCreature(c domApi.CreatureModel) domApi.CreatureModel {
+	return u.repo.Insert(c)
 }
--- api/usecase/spellUsecase.go
+++ api/usecase/spellUsecase.go
@@ -1,13 +1,33 @@
 package api
 
 import (
+	"errors"
+
+	domApi "github.com/caioleone/go-grimoire-api/api/domain"
 	repoApi "github.com/caioleone/go-grimoire-api/api/repository"
 )
 
 type SpellUsecase struct {
-	repo repoApi.MemorySpellRepository
+	repo repoApi.SpellRepository
 }
 
-func NewSpellUseCase(repo repoApi.MemorySpellRepository) *SpellUsecase {
+func NewSpellUseCase(repo repoApi.SpellRepository) *SpellUsecase {
 	return &SpellUsecase{repo: repo}
+}
+
+func (u *SpellUsecase) CreateSpell(spell domApi.SpellModel) (domApi.SpellModel, error) {
+
+	if len(spell.Name) < 2 {
+		return domApi.SpellModel{}, errors.New("Invalid Name")
+	}
+
+	if spell.ManaCost <= 0 {
+		return domApi.SpellModel{}, errors.New("Invalid Mana Cost")
+	}
+
+	return u.repo.Insert(spell), nil
+}
+
+func (u *SpellUsecase) GetAll() []domApi.SpellModel {
+	return u.repo.FindAll()
 }
