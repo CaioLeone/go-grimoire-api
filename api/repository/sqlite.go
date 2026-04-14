5441e95b539074f8d3f0b9f44b4933eac50ae8f0
Caio <caio_leone@hotmail.com>
2026-04-14 09:26:44 -0300

    feat: sqlite3 dependency

---
+++ api/repository/sqlite.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func NewSQLiteConnection() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./grimoire.db")
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func InitDB(db *sql.DB) error {
+	querySpell := `
+		CREATE TABLE IF NOT EXISTS spells (
+			id TEXT PRIMARY KEY,
+			name TEXT,
+			description TEXT,
+			element TEXT,
+			mana_cost INTEGER
+		);`
+
+	queryCreature := `
+		CREATE TABLE IF NOT EXISTS creature (
+			id TEXT PRIMARY KEY,
+			name TEXT,
+			description TEXT,
+			attack INTEGER,
+			defence INTEGER,
+			hp INTEGER
+		);`
+
+	queryRelation := `
+		CREATE TABLE IF NOT EXISTS creature_spells (
+			creature_id TEXT,
+			spell_id TEXT
+		);`
+
+	_, err := db.Exec(querySpell)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(queryCreature)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(queryRelation)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
