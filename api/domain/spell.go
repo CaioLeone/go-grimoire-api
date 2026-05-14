43bbd687ab12f5d060e2d4eaec258aa94ad9fa17
Caio <caio_leone@hotmail.com>
2026-05-14 14:33:45 -0300

    fix: spellmodel added type and power variables

--- api/domain/spell.go
+++ api/domain/spell.go
@@ -6,6 +6,8 @@ type SpellModel struct {
 	ID          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
-	ManaCost    int     `json:"mana_cost"`
+	ManaCost    int       `json:"mana_cost"`
 	Element     string    `json:"element"`
+	Type        string    `json:"type"`
+	Power       int       `json:"power"`
 }
